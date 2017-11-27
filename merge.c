75ed37b421f9a76df5f64bce1a2a855add2652f0
ForeXis <34023436+ForeXis@users.noreply.github.com>
2017-11-28 01:40:25 +0700

    а вот и коммит :D

--- merge.c
+++ merge.c
@@ -13,18 +13,26 @@ data *arr;
 uint64_t len;
 
 int main(void) {
-printf("Array length: ");
-scanf("%lu", &len);
-if(len < 2) {
-printf("Length >= 2\n");
-return 1;
-}
+	printf("Array length: ");
+	scanf("%lu", &len);
+	if(len < 2) {
+	  printf("Length >= 2\n");
+	  return 1;
+	 }
 
 srand(time(NULL));
 arr = malloc(len * sizeof(data));
 for(uint64_t i = 0; i < len; ++i)
-arr[i] = rand() / (RAND_MAX / (RMAX - 1));
+  arr[i] = rand() / (RAND_MAX / (RMAX - 1));
+  
+  array_print(arr, len);  
 
 free(arr); 
 return 0;
+}
+
+void array_print(const data *a, uint64_t n) {
+  for(uint64_t i = 0; i < n; ++i)
+    printf("%u\t", a[i]);
+  printf("\n");
 }
