83b47957e816dee16b542c634e906d5cd81eb04f
Benderos <Bender197111@gmail.com>
2017-11-28 01:12:11 +0700

    main

--- merge.c
+++ merge.c
@@ -5,10 +5,17 @@
 #include <time.h>
 
 
+typedef uint64_t data;
 
+uint64_t len;
 
 int main(void) {
   printf("Array length: ");
-  
+  scanf("%lu", &len);
+    if(len < 2) {
+    printf("Length >= 2\n");
+    return 1;
+  }
+
   return 0;
 }
