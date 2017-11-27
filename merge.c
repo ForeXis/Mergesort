a2e0e8babb7e0a7303d010f6d2a61606360b92f6
VeraChertkova <33719887+VeraChertkova@users.noreply.github.com>
2017-11-28 01:30:25 +0700

    main1

--- merge.c
+++ merge.c
@@ -5,17 +5,26 @@
 #include <time.h>
 
 
+#define RMAX 100
+
 typedef uint64_t data;
 
+data *arr;
 uint64_t len;
 
 int main(void) {
-  printf("Array length: ");
-  scanf("%lu", &len);
-    if(len < 2) {
-    printf("Length >= 2\n");
-    return 1;
-  }
+printf("Array length: ");
+scanf("%lu", &len);
+if(len < 2) {
+printf("Length >= 2\n");
+return 1;
+}
 
-  return 0;
+srand(time(NULL));
+arr = malloc(len * sizeof(data));
+for(uint64_t i = 0; i < len; ++i)
+arr[i] = rand() / (RAND_MAX / (RMAX - 1));
+
+free(arr); 
+return 0;
 }
