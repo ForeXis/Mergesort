8f090460a25175d002d693e0a1c91b4bde6019ff
ForeXis <34023436+ForeXis@users.noreply.github.com>
2017-12-01 08:31:56 +0700

    1

--- test/test.c
+++ test/test.c
@@ -34,6 +34,40 @@ CTEST(suite1, test1){
   }
   
 }
+CTEST(suite2, test2){
+
+  // Given
+   uint64_t arr[20] = {0, 0, 7, 3, 18, 9, 23, 25, 27, 32, 27, 36, 45, 49, 63, 65, 81, 86, 94, 99};
+  const uint64_t len = 20;
+  
+ 
+ merge_sort(arr, len);
+
+  // Then
+  for(uint64_t i = 0, j = 1; i < len; ++i){
+  	if (arr[i] < arr[i+1]){
+  		j++ ;
+	  }
+}
+
+  	printf("compare %d =? %d\n", i , j);
+  ASSERT_EQUAL(i, j);
+}
+  
+  
+
+
+
+
+  
+  for(uint64_t i = 0; i < len; ++i){
+  	printf("compare %d %d =? %d\n", i , b[i], arr[i]);
+  ASSERT_EQUAL(b[i], arr[i]);
+  
+
+  }
+  
+}
   
 
 
